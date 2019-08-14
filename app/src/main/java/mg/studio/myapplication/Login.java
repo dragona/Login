a2cb5201fce938e2315632333f11c69eb8449f5f
Feilong <feilong.dragona@gmail.com>
2019-08-15 04:29:41 +0800

    migrate t androidx

--- app/src/main/java/mg/studio/myapplication/Login.java
+++ app/src/main/java/mg/studio/myapplication/Login.java
@@ -1,14 +1,12 @@
 package mg.studio.myapplication;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
